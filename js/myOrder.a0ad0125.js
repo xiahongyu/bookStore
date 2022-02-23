1eb114a08d60633b9a16da48a94b509785e2712a
xiahongyu <96030681+xiahongyu@users.noreply.github.com>
2022-02-23 17:43:26 +0800

    Delete js directory

--- js/myOrder.a0ad0125.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["myOrder"],{"72ba":function(e,t,n){"use strict";n.r(t);var i=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"myContent"},[n("keep-alive",[n("router-view")],1)],1)},r=[],s={},c=s,u=(n("7d29"),n("2877")),a=Object(u["a"])(c,i,r,!1,null,"4f7f4008",null);t["default"]=a.exports},"7d29":function(e,t,n){"use strict";n("b5bf")},b5bf:function(e,t,n){}}]);
-//# sourceMappingURL=myOrder.a0ad0125.js.map
